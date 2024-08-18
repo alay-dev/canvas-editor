b8244563c14e7a9ae1fda29d7e5d470f2425b663
Alay Naru <alay@zocket.com>
2024-08-18 12:14:28 +0530

    next config updated

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  webpack: (config) => {
-    config.externals.push({
-      "utf-8-validate": "commonjs utf-8-validate",
-      bufferutil: "commonjs bufferutil",
-      canvas: "commonjs canvas",
-    });
-    // config.infrastructureLogging = { debug: /PackFileCache/ };
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
+    config.externals.push({ canvas: "commonjs canvas" });
     return config;
   },
 };
