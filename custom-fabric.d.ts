eda925ea1abcda2258e68248f01fadc28abd5c93
Alay Naru <alay@zocket.com>
2024-08-19 20:51:01 +0530

    build fix

--- custom-fabric.d.ts
+++ custom-fabric.d.ts
@@ -1,4 +1,4 @@
-import { fabric } from "fabric";
+import * as fabric from "fabric";
 import { Border } from "./types/c-image";
 
 declare module "fabric" {
--- next.config.js
+++ next.config.js
@@ -5,6 +5,15 @@ const nextConfig = {
     config.externals.push({ canvas: "commonjs canvas" });
     return config;
   },
+  // webpack: (config) => {
+  //   config.externals.push({
+  //     "utf-8-validate": "commonjs utf-8-validate",
+  //     bufferutil: "commonjs bufferutil",
+  //     canvas: "commonjs canvas",
+  //   });
+  //   // config.infrastructureLogging = { debug: /PackFileCache/ };
+  //   return config;
+  // },
 };
 
 module.exports = nextConfig;
