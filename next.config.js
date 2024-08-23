0d6e1cdd37985e03974aa021b95e801ba5ba7556
Alay Naru <alay@zocket.com>
2024-08-23 21:22:20 +0530

    fix

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
-    config.externals.push({ canvas: "commonjs canvas" });
-    return config;
-  },
+  // webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
+  //   config.externals.push({ canvas: "commonjs canvas" });
+  //   return config;
+  // },
   // webpack: (config) => {
   //   config.externals.push({
   //     "utf-8-validate": "commonjs utf-8-validate",
