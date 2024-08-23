ed433b54954ec27cccec0d525d6343e32e28fa01
Alay Naru <alay@zocket.com>
2024-08-23 21:18:17 +0530

    fix

--- next.config.js
+++ next.config.js
@@ -1,15 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  webpack: (config) => {
-    config.externals.push({
-      "utf-8-validate": "commonjs utf-8-validate",
-      bufferutil: "commonjs bufferutil",
-      canvas: "commonjs canvas",
-    });
-    // config.infrastructureLogging = { debug: /PackFileCache/ };
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
+    config.externals.push({ canvas: "commonjs canvas" });
     return config;
   },
+  // webpack: (config) => {
+  //   config.externals.push({
+  //     "utf-8-validate": "commonjs utf-8-validate",
+  //     bufferutil: "commonjs bufferutil",
+  //     canvas: "commonjs canvas",
+  //   });
+  //   // config.infrastructureLogging = { debug: /PackFileCache/ };
+  //   return config;
+  // },
 };
 
 module.exports = nextConfig;
